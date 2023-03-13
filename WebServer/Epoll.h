c8e45f8cb0f160f624060f51d1ecaeed098eb4ae
believer <50709243+Breveile@users.noreply.github.com>
2023-03-13 16:01:42 +0800

    Update Epoll.h

--- WebServer/Epoll.h
+++ WebServer/Epoll.h
@@ -8,16 +8,16 @@
 #include "Channel.h"
 #include "HttpData.h"
 #include "Timer.h"
-
+// Epoll主要用于对系统原始的epoll库进行封装，以适应于我们现在的业务逻辑，主要的功能还是创建epoll，以及一些对epoll的操作（注册、修改、删除等等）
 
 class Epoll {
  public:
-  Epoll();
+  Epoll();// 在构造函数中创建一个epoll句柄
   ~Epoll();
-  void epoll_add(SP_Channel request, int timeout);
+  void epoll_add(SP_Channel request, int timeout);// 将需要监听的文件描述符绑定到刚才创建的epoll句柄上
   void epoll_mod(SP_Channel request, int timeout);
   void epoll_del(SP_Channel request);
-  std::vector<std::shared_ptr<Channel>> poll();
+  std::vector<std::shared_ptr<Channel>> poll(); // 获取epoll句柄监听的活跃事件列表
   std::vector<std::shared_ptr<Channel>> getEventsRequest(int events_num);
   void add_timer(std::shared_ptr<Channel> request_data, int timeout);
   int getEpollFd() { return epollFd_; }
